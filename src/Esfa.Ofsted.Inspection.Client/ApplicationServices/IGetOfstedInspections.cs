47e3131044ab7ff43f4a20d8bc22a9f429d8745a
MarkFCain <mark.cain@alimar.co.uk>
2017-10-17 09:30:47 +0100

    CI-404 added xml comment to GetInspections GetAll

--- src/Esfa.Ofsted.Inspection.Client/ApplicationServices/IGetOfstedInspections.cs
+++ src/Esfa.Ofsted.Inspection.Client/ApplicationServices/IGetOfstedInspections.cs
@@ -3,6 +3,9 @@ using Esfa.Ofsted.Inspection.Types;
 
 namespace Esfa.Ofsted.Inspection.Client.ApplicationServices
 {
+    /// <summary>
+    /// Get All available Ofsted Inspection Details
+    /// </summary>
     public interface IGetOfstedInspections
     {
         InspectionsDetail GetAll();
